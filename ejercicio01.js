297280f510972ae1fe7353624a69b3748f4bf378
salazarfatima5648-lab <salazarfatima.5648@gmail.com>
2026-07-14 23:53:07 -0600

    Ejercicio 01: ficha de datos personales

---
+++ ejercicio01.js
@@ -0,0 +1,17 @@
+// Variables que cambian//
+let nombrecompleto = "Fatima salazar";
+let edad = 24;
+let promedio = 8.5;
+
+// variables que no cambian//
+const matricula = "USXCU687"
+const carrera = "Sistemas Computacioanles Administrativos";
+
+//impresion de la ficha de datos con template litearals//
+
+console.log(`Ficha de datos personales:
+Nombre: ${nombrecompleto}
+Edad: ${edad}
+Matrícula: ${matricula}
+Carrera: ${carrera}
+Promedio: ${promedio}`);

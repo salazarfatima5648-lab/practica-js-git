f0f07a8b68ad3ff0a2982361591868a35ddd00b0
salazarfatima5648-lab <salazarfatima.5648@gmail.com>
2026-07-16 00:35:27 -0600

    Ejercicio 07: Comparaciones: predice y verifica

---
+++ ejercicio07.js
@@ -0,0 +1,31 @@
+// Ejercicio 07 - Comparaciones: predice y verifica
+
+// 1. Comparación: 10 == "10"
+
+console.log(10 == "10"); 
+
+//  Comparación: 10 === "10"
+
+console.log(10 === "10"); 
+
+//  Comparación: 0 == false
+
+console.log(0 == false); 
+
+//  Comparación: 0 === false
+
+console.log(0 === false); 
+
+// Comparación: "abc" < "abd"
+
+console.log("abc" < "abd"); 
+
+//  Comparación: 5 !== 5.0
+// Prediccion false
+console.log(5 !== 5.0); 
+
+
+/*
+Explicación de la diferencia entre igualdad simple y estricta:
+la igualdad simple compara un solo valor lanza un valor verdadero en cambio la igualdad estricta compara valor y tipo de dato
+*/
